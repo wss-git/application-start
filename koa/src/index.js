a8d776a3df7408199e78266c147387a8ee85938d
wss-git <wssgryx@163.com>
2021-04-07 11:28:15 +0800

    feat: add koa

---
+++ koa/src/index.js
@@ -0,0 +1,26 @@
+const Koa = require('koa');
+const app = new Koa();
+
+// x-response-time
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
+  ctx.set('Content-Type', 'text/html;charset=utf-8');
+});
+
+// logger
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}`);
+});
+
+// response
+app.use(async ctx => {
+  ctx.body = 'Hello World';
+});
+
+app.listen(9000);
